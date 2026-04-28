be04215a1f98cc6974cee8eb641efc9df4cd24b3
realRickyNguyen <128347911+realRickyNguyen@users.noreply.github.com>
2026-04-28 11:01:36 +1000

    Delete crosspoe-main directory

--- crosspoe-main/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .crosspoe import CrossPoE, kl_divergence_gaussian, kl_divergence_two_gaussians
-from .blocks import fc_block, count_params
-from .decoders import MethylDecoder, MIRNADecoder, RNADecoder
-from .encoders import MethylEncoder, MIRNAEncoder, RNAEncoder
-from .poe import ProductOfExperts, reparameterise
-from .survival import SurvivalHead, cox_partial_likelihood_loss
-from .translation import CrossModalTranslator, LatentTranslationHead, TranslationGateNetwork
